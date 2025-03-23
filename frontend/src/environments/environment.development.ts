3132cbcb2adacc4259ae72e5b730bee45cc7da34
krimoi45 <krimoi@gmail.com>
2025-03-23 23:23:50 +0100

    Configuration d'environnement de développement

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  socketUrl: 'http://localhost:3000',
+  aiServiceUrl: 'http://localhost:5000',
+  version: '1.0.0-dev'
+};
